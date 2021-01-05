5bc0e12ef0169e0c78c6d615ad15e40c59391339
YOUR NAME <YOUR EMAIL ADDRESS>
2021-01-04 17:46:42 -0800

    sidebar option foundation added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/HeaderFolder/Header";
+import Sidebar from "./components/SidebarFolder/Sidebar";
 
 function App() {
   return (
     <div className="pp">
       <Header />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/SidebarFolder/Sidebar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Sidebar.css";
+import { Button } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import InboxIcon from "@material-ui/icons/Inbox";
+import SidebarOption from "../SidebarOptionFolder/SidebarOption";
+import StarIcon from "@material-ui/icons/Star";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <Button
+        startIcon={<AddIcon fontSize="large" />}
+        className="sidebar__compose"
+      >
+        Compose
+      </Button>
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={54}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" number={54} />
+    </div>
+  );
+};
+
+export default Sidebar;
