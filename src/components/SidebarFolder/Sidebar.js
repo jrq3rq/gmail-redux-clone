d296650bfa4a784b28b0e50f9a3561a325b7d485
YOUR NAME <YOUR EMAIL ADDRESS>
2021-01-08 17:12:03 -0800

    sidebar icons added

--- src/components/SidebarFolder/Sidebar.js
+++ src/components/SidebarFolder/Sidebar.js
@@ -5,6 +5,11 @@ import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
 import SidebarOption from "../SidebarOptionFolder/SidebarOption";
 import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const Sidebar = () => {
   return (
@@ -22,6 +27,11 @@ const Sidebar = () => {
         selected={true}
       />
       <SidebarOption Icon={StarIcon} title="Starred" number={54} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={54} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" number={54} />
+      <SidebarOption Icon={NearMeIcon} title="Sent" number={54} />
+      <SidebarOption Icon={NoteIcon} title="Draft" number={54} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
     </div>
   );
 };
