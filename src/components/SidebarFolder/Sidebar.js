70bcb112f89ea0d71596f8f626ceed67a7f985e5
YOUR NAME <YOUR EMAIL ADDRESS>
2021-01-08 17:19:24 -0800

    footer started

--- src/components/SidebarFolder/Sidebar.js
+++ src/components/SidebarFolder/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Sidebar.css";
-import { Button } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
 import SidebarOption from "../SidebarOptionFolder/SidebarOption";
@@ -10,6 +10,9 @@ import LabelImportantIcon from "@material-ui/icons/LabelImportant";
 import NearMeIcon from "@material-ui/icons/NearMe";
 import NoteIcon from "@material-ui/icons/Note";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
 
 const Sidebar = () => {
   return (
@@ -32,6 +35,20 @@ const Sidebar = () => {
       <SidebarOption Icon={NearMeIcon} title="Sent" number={54} />
       <SidebarOption Icon={NoteIcon} title="Draft" number={54} />
       <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 };
